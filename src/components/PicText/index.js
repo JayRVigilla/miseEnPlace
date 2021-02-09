de4d65d3542c36f413065dd3bc6d5c14a3615ab8
Jay Vigilla <JayRVigilla@gmail.com>
2021-02-09 13:49:42 -0800

    PicText
    added props for right and img.width

--- src/components/PicText/index.js
+++ src/components/PicText/index.js
@@ -1,21 +1,27 @@
 import React from 'react'
 /**
+ * Container for picture and div holding an h2 and text
  *
- * @param {Object} img {src, alt}
+ * @param {Object} img {src, alt, width(str)}
  * @param {String} title
  * @param {Array} text [str, str,..., str]
+ * @param {Boolean} right when true, positions image to the right
  */
 
-
-
-export default function PicText({ img, title, text }) {
+export default function PicText({ img, title, text, right }) {
   function makePs(arr) {
     arr.forEach( str => <p>{str}</p>);
   }
 
+  const pos = right ? 'pic-right' : ''
+
+  const styleObj = {
+    width: `${img.width}%`
+  }
+
   return (
-    <section className="pic-text">
-      <img src={img.src} alt={img.alt}/>
+    <section className={`pic-text ${pos}`}>
+      <img src={img.src} alt={img.alt} style={styleObj}/>
     <div className="desc">
         <h2>{title}</h2>
         {makePs(text)}
