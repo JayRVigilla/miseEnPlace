9b756314bced49a19ac05af041ec9a543160c56b
Jay Vigilla <JayRVigilla@gmail.com>
2021-02-09 13:56:44 -0800

    cleaned out files from previous projects

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-import Img from './Img';
 import NavBar from './NavBar';
-import HeaderLink from './HeaderLink';
-import Banner from './banner.jpg';
-import messages from './messages';
 
 function Header() {
   return (
     <div>
-      <Img src={Banner} alt="Macrame Wall Hanging - Strings" />
       <NavBar>
-        <HeaderLink to="/">
-          <FormattedMessage {...messages.home} />
-        </HeaderLink>
-        <HeaderLink to="/add">
-          <FormattedMessage {...messages.addString} />
-        </HeaderLink>
+
       </NavBar>
     </div>
   );
